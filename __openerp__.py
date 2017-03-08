902f3c6488281f2cbfd58e5e752a96ab14a7638b
Raul Ovalle <raul@Rauls-MacBook-Pro.local>
2017-03-08 10:35:47 -0400

    monedas por billetes

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
 'name': "POS Dominican currencies",
-'description' : "Agrega al pos las denominaciones de las monedas Dominicanas",
+'description' : "Agrega al pos las denominaciones de los billetes Dominicanos",
 'version' : "10.0.0",
 'author' : "Raul Ovalle, xmarts.com",
 'depends' : ['point_of_sale'],
