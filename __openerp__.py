152bf8741e55c8822c7fe10dcbf03eb6fdbf6ad2
Raul Ovalle <raul@Rauls-MacBook-Pro.local>
2017-03-08 10:29:07 -0400

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+'name': "POS Dominican currencies",
+'description' : "Agrega al pos las denominaciones de las monedas Dominicanas",
+'version' : "10.0.0",
+'author' : "Raul Ovalle, xmarts.com",
+'depends' : ['point_of_sale'],
+'data': [],
+'qweb': ['static/src/xml/pos.xml'],
+'installable' : True,
+}
