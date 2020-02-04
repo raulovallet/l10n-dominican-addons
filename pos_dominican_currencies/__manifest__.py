e85eb1dcefe00dd73f07b40d47699c135a409f44
Raul Ovalle <raulovallet@gmail.com>
2020-02-03 22:28:07 -0600

    [REF] migration to 12.0

---
+++ pos_dominican_currencies/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "POS Dominican currencies",
+    'description': "Agrega al pos las denominaciones de los billetes Dominicanos",
+    'version': "12.0.0",
+    'author': "Raul Ovalle",
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'installable' : True,
+}
